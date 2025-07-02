72933aaa5f54cd32e8717a5824a9a79358d392ae
FCDigipen <franklin.chen@digipen.edu>
2025-07-02 08:39:27 -0700

    made the ball bounce off the sides of the screen

--- Assets/Scripts/BallMotor.cs
+++ Assets/Scripts/BallMotor.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class BallMotor : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera cam;
     public float speed = 5f;
     private Vector2 velocity;
     void Start()
@@ -19,6 +21,20 @@ public class BallMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Bounce();
         transform.position += (transform.up * velocity.y + transform.right * velocity.x) * Time.deltaTime;
+    }
+
+    // bounce checks!
+    private void Bounce() {
+        Vector2 positionOnScreen = cam.WorldToScreenPoint(transform.position);
+        if((positionOnScreen.x <= 0 && velocity.x < 0) || (positionOnScreen.x >= cam.pixelWidth && velocity.x > 0)) // x bounce
+        {
+            velocity.x *= -1;
+        } 
+        if((positionOnScreen.y <= 0 && velocity.y < 0) || (positionOnScreen.y >= cam.pixelHeight && velocity.y > 0)) // y bounce
+        {
+            velocity.y = -velocity.y;
+        } 
     }
 }
