17b124e094df81613c02b35a374abc9975557352
FCDigipen <franklin.chen@digipen.edu>
2025-07-02 08:27:31 -0700

    added random initalization velocity to the ball

--- Assets/Scripts/BallMotor.cs
+++ Assets/Scripts/BallMotor.cs
@@ -1,19 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BallMotor : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 5f;
+    private Vector2 velocity;
     void Start()
     {
-        
+        velocity = new Vector2(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f,1f));
+        velocity.Normalize();
+        velocity *= speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.up * Time.deltaTime;
-        Debug.Log(transform.position);
+        transform.position += (transform.up * velocity.y + transform.right * velocity.x) * Time.deltaTime;
     }
 }
