67768bcdc17cf73ea9d73ab27ea50d383561878b
FCDigipen <franklin.chen@digipen.edu>
2025-07-02 09:51:11 -0700

    added BounceCounter.cs to ScoreCounter

---
+++ Assets/Scripts/BounceCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BounceCounter : MonoBehaviour
+{
+    private int bounces = 0;
+    private TMP_Text text;
+
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();   
+    }
+
+    public void updateCounter() {
+        ++bounces;
+        text.text = $"Bounces: {bounces}";
+    }
+}
